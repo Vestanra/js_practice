7b9f10f9da54455d2534ec00ffa52054c1225d8c
Vestanra <35749970+Vestanra@users.noreply.github.com>
2023-05-04 21:35:12 +0300

    Update less3_1.js

--- less3_1.js
+++ less3_1.js
@@ -116,4 +116,98 @@ for (const value of values) {
 console.log(totalFeedbackNew);
 
 
+const friends = [
+    { name: 'Mango', online: false, },
+    { name: 'Kiwi', online: true, },
+    { name: 'Poly', online: true, },
+    { name: 'Ajax', online: false, },
+    { name: 'Anna', online: false, }, 
+];
+// friends[1].newprop = 555;
+console.table(friends);
+
+// for (const friend of friends) {
+//     console.log(friend.name);
+
+//     friend.newprop = 555;
+// };
+
+// console.table(friends);
+
+const findFriendByName = function (allFriends, name) {
+    for (const friend of friends) {
+        console.log(friend);
+        console.log(friend.name);
+       
+        if (friend.name === name) {
+            return 'the name was found';
+        } 
+    }
+    return 'the name was not found'
+};
+console.log(findFriendByName(friends, 'Poly'));
+console.log(findFriendByName(friends, 'Chelsy'));
+
+function getAllNames(allFriends, name) {
+
+    const friendsArray = [];
+    for (const friend of friends) {
+        friendsArray.push(friend.name);
+    }
+    // console.log(friendsArray);
+    return  friendsArray;
+}
+console.log(getAllNames());
+
+//повертаємо всіх онлайн
+function getOnlineFriends(allFriends) {
+    const onlineFriends = [];
+    for (const friend of friends) {
+        if (friend.online) { //(friend.online === true) так не писати
+            onlineFriends.push(friend);
+        }
+    }
+    return onlineFriends;
+}
+console.log(getOnlineFriends());
+
+
+//повертаємо всіх офлайн
+function getOfflineFriends(allFriends) {
+    const offlineFriends = [];
+    for (const friend of friends) {
+        if (!friend.online) {
+            offlineFriends.push(friend);
+        }
+    }
+    return offlineFriends;
+}
+console.log(getOfflineFriends());
+
+const getFriendsByOnlineStatus = function (allFriends) {
+    const friendsByStatus = {
+        online: [],
+        offline: [],
+    };
+
+    for (const friend of friends) {
+        if (friend.online) {
+            friendsByStatus.online.push(friend);
+            continue;
+        }
+        friendsByStatus.offline.push(friend);
+    }
+    return friendsByStatus;
+};
+
+console.log(getFriendsByOnlineStatus(friends));
+
+
+const x = {
+    a: 1,
+    b: 2,
+    c: 50,
+    d: 100,
+};
+console.log(Object.keys(x).length);
 
