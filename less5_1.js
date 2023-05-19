59c260d7298b2ebdc1ac38530f364a708cca7054
Vestanra <35749970+Vestanra@users.noreply.github.com>
2023-05-19 22:06:16 +0300

    11

--- less5_1.js
+++ less5_1.js
@@ -128,14 +128,100 @@ class Ccar {
         this.items.push(newItem);
     }
     removeItem(itemToRemove) {
-        this.items.reduce([], element => {
-            
-        })
+      this.items = this.items.filter(element => element !== itemToRemove);
     }
   }
   const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
   console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
   storage.addItem("Droid");
   console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-//   storage.removeItem("Prolonger");
-//   console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]  
+  storage.removeItem("Prolonger");
+  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+  
+
+
+class StringBuilder {
+  constructor (initialValue) {
+    this.value = initialValue;
+  }
+  getValue() {
+    return this.value;
+  }
+  padEnd(str) {
+    this.value = this.value + str;
+  }
+  padStart(str) {
+    this.value = str + this.value;
+  }
+  padBoth(str) {
+    this.value = str + this.value + str;
+  }
+}
+
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+
+
+class Carr {
+  // Change code below this line
+#price;
+  MAX_PRICE = 50000;
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice <= this.MAX_PRICE) {
+       this.#price = newPrice;
+    }
+
+  }
+  // Change code above this line
+}
+
+const audi = new Carr({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
+
+
+class User1 {
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+// Change code below this line
+class Admin extends User {
+  static AccessLevel = {
+  BASIC: "basic",
+  SUPERUSER: "superuser"
+  }
+  constructor(email) {
+    super(email);
+  }
+}
+
