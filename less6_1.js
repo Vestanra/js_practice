95d376934774e74d903b003e18ffee53f7c53a59
Vestanra <35749970+Vestanra@users.noreply.github.com>
2023-05-22 21:05:48 +0300

    Update less6_1.js

--- less6_1.js
+++ less6_1.js
@@ -9,3 +9,43 @@ let technologiesMarkup = technologies.map(technology => `<li>${technology}</li>`
 
 console.log(technologiesMarkup);
 technologiesEl.innerHTML = technologiesMarkup;
+
+
+
+const colors = [
+    {
+        label: 'red',
+        color: '#FF0000',
+    },
+    {
+        label: 'green',
+        color: '#00FF00',
+    },
+    {
+        label: 'blue',
+        color: '#0000FF',
+    },
+    {
+        label: 'yellow',
+        color: '#FFFF00',
+    },
+];
+const colorsButtons = colors.map(color => {
+    let colorButton = document.createElement('button');
+    colorButton.textContent = color.label;
+    colorButton.style.backgroundColor = color.color;
+
+    return colorButton;
+});
+
+// bodyEl.append(...colorsButtons);
+// colorsButtons.forEach(btn => bodyEl.append(btn));
+colors
+    .forEach(color => {
+        let colorButton = document.createElement('button');
+        colorButton.textContent = color.label;
+        colorButton.style.backgroundColor = color.color;
+        bodyEl.append(colorButton);
+    });
+    
+    
